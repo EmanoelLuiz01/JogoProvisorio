109dd1f3bd15cdcbe441a7c2a44b5558c0b27de3
EmanoelLuiz01 <133930585+EmanoelLuiz01@users.noreply.github.com>
2023-06-13 18:07:34 -0300

    primeiro

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Jogar from './jogar.svelte';
+	import Sobre from './sobre.svelte';
+	let pagina = "menu";
+
+</script>
+
+<main>
+	
+</main>
+
+<style>
+	.container {
+      text-align: center;
+    }
+</style>
+
+	<div class="container">
+	  <button on:click={() => pagina = "jogar"} class="jogar">Jogar</button>
+	</div>
+	<div>
+		<button on:click={() => pagina = "sobre"}>sobre</button>
+	</div>
+{#if pagina == "jogar"}
+	<Jogar />
+{:else if pagina == "sobre"}
+	<Sobre />		
+{/if}
+
+
